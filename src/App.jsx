c06b29d945b4d00bcf594c909ef3a06b4e0a78a8
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-07-02 02:14:35 +0530

    add vite project w/tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="flex justify-center items-center text-white bg-black/75 h-screen">
+            <h1 className="text-5xl font-serif font-semibold">Velovista</h1>
+        </div>
+    );
+}
+
+export default App;
